9a5acbad6875face9b737284d3fc029a024a63ca
Alice0Gabriela <119084588+Alice0Gabriela@users.noreply.github.com>
2022-11-25 22:07:54 -0300

    Update public class Main {.java

--- public class Main {.java
+++ public class Main {.java
@@ -9,138 +9,58 @@ struct Fila {
 	float *dados;
 	int primeiro;
 	int ultimo;
-	int nItens; 
+	package pilha;
 
-};
+public class projeto {
 
-void criarFila( struct Fila *f, int c ) { 
+	public static void main(String[] args) {
 
-	f->capacidade = c;
-	f->dados = (float*) malloc (f->capacidade * sizeof(float));
-	f->primeiro = 0;
-	f->ultimo = -1;
-	f->nItens = 0; 
+		Pilha<Integer> par = new Pilha<Integer>();
+		Pilha<Integer> ímpar = new Pilha<Integer>();
 
-}
+		Scanner scar = new Scanner(System.in);
 
-void inserir(struct Fila *f, int v) {
+		for (int i=1; i<=30); i++){
 
-	if(f->ultimo == f->capacidade-1)
-		f->ultimo = -1;
+			int num = scan.nextInt();
 
-	f->ultimo++;
-	f->dados[f->ultimo] = v; // incrementa ultimo e insere
-	f->nItens++; // mais um item inserido
+			if (num == 0) {
 
-}
+				//pilha par
 
-int remover( struct Fila *f ) { // pega o item do comeÃ§o da fila
+				Integer desempilhado = par.desempilha();
 
-	int temp = f->dados[f->primeiro++]; // pega o valor e incrementa o primeiro
-
-	if(f->primeiro == f->capacidade)
-		f->primeiro = 0;
-
-	f->nItens--;  // um item retirado
-	return temp;
-
-}
-
-int estaVazia( struct Fila *f ) { // retorna verdadeiro se a fila estÃ¡ vazia
-
-	return (f->nItens==0);
-
-}
-
-int estaCheia( struct Fila *f ) { // retorna verdadeiro se a fila estÃ¡ cheia
-
-	return (f->nItens == f->capacidade);
-}
-
-void mostrarFila(struct Fila *f){
-
-	int cont, i;
-
-	for ( cont=0, i= f->primeiro; cont < f->nItens; cont++){
-
-		printf("%.2f\t",f->dados[i++]);
-
-		if (i == f->capacidade)
-			i=0;
-
-	}
-	printf("\n\n");
-
-}
-
-void main () {
-
-	int opcao, capa;
-	float valor;
-	struct Fila umaFila;
-
-	// cria a fila
-	printf("\nCapacidade da fila? ");
-	scanf("%d",&capa);
-	criarFila(&umaFila, capa);
-
-	// apresenta menu
-	while( 1 ){
-
-		printf("\n1 - Inserir elemento\n2 - Remover elemento\n3 - Mostrar Fila\n0 - Sair\n\nOpcao? ");
-		scanf("%d", &opcao);
-
-		switch(opcao){
-
-			case 0: exit(0);
-
-			case 1: // insere elemento
-				if (estaCheia(&umaFila)){
-
-					printf("\nFila Cheia!!!\n\n");
-
+				if (desempilhado == null) {
+					System.out.println("Pilha par vazia");
+				} else {
+					System.out.println(Desempilhado da pilha par: " +desempilhado);")
 				}
-				else {
+				//pilha impar
 
-					printf("\nValor do elemento a ser inserido? ");
-					scanf("%f", &valor);
-					inserir(&umaFila,valor);
+				Integer desempilhado = impar.desempilha();
 
+				if (desempilhado == null){
+					System.out.println("Pilha impar vazia");
+				} else {
+					System.out.println(Desempilhado da pilha impar: " +desempilhado);")
 				}
+			}
 
-				break;
+			if(num % 2 == 0){
+				System.out.println("Número impar, empilhando na pilha par: " + num);
 
-			case 2: // remove elemento
-				if (estaVazia(&umaFila)){
-
-					printf("\nFila vazia!!!\n\n");
-
-				}
-				else {
-
-					valor = remover(&umaFila);
-					printf("\n%1f removido com sucesso\n\n", valor) ; 
-
-				}
-				break;
-
-				case 3: // mostrar fila
-					if (estaVazia(&umaFila)){
-
-						printf("\nFila vazia!!!\n\n");
-
+				while (!par.estaVazia()) {
+					System.ou.println("Desempilhando todos os números da pilha par");
+					
+					while (!impar.estaVazia()){
+						System.out.println("Desempilhando da pilha ímpar")
+						
 					}
-					else {
-
-						printf("\nConteudo da fila => ");
-						mostrarFila(&umaFila);
-
-					}
-					break;
-
-				default:
-					printf("\nOpcao Invalida\n\n");
-
+				}
+			}
 		}
+
+
 	}
+
 }
